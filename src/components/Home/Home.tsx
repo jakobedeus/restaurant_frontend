d80ac7cfc6c30a7d4f871bd8f272773bd6f37ee4
cristina barbagallo <cris.barbagallo@gmail.com>
2019-08-28 12:44:03 +0200

    Create basic homepage

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,7 +5,14 @@ class Home extends React.Component<{}, {}>  {
 
     render() {
         return(
-            <div>Hej</div>
+            <div className="hero-container">
+                <div className="title-container">
+                    <div className="row">
+                        <h1>No Name Restaurant</h1>
+                        <h2>Tuesday - Sunday &emsp;&emsp; 12 am - Midnight </h2>
+                    </div>
+                </div>
+            </div>
         )
     }
 

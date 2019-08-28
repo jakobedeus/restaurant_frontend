daf708b374dc3e79235b5f1ae0007d9bbaf5a283
cristina barbagallo <cris.barbagallo@gmail.com>
2019-08-28 17:12:52 +0200

    Create basic structure and style for add reservation form

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,11 +1,61 @@
 import React from "react";
 import './scss/Admin.scss';
+import { FaPlus } from 'react-icons/fa';
 
-class Admin extends React.Component<{}, {}>  {
+interface IProps {
+	formDisplay: any;
+}
 
+
+class Admin extends React.Component<{}, IProps>  {
+	constructor(props: any) {
+    super(props)
+    this.state = {
+      formDisplay: false,
+		}
+		
+		this.toggleForm = this.toggleForm.bind(this);
+	}
+
+	toggleForm() {
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    })
+  }
+	
+	
     render() {
         return(
-            <div>Admin works</div>
+					<div className={
+						'form-container ' +
+						(this.state.formDisplay ? '' : 'add-reservation')
+					}>
+						<div className="res-addheading" onClick={this.toggleForm}>
+							<h2> <FaPlus /> Add Reservation</h2>
+						</div>
+
+						<div className="form-body">
+							<form>
+								<input type="text" name="name" placeholder="Full Name" />
+								<input type="text" name="phone" placeholder="Phone Number" />
+								<input type="email" name="email" placeholder="Email" />
+								<input type="date" name="date" placeholder="Date" />
+
+								<div className="inputGroup">
+									<input id="radio1" name="radio" type="radio"/>
+									<label htmlFor="radio1">18:00</label>
+								</div>
+								<div className="inputGroup">
+									<input id="radio2" name="radio" type="radio"/>
+									<label htmlFor="radio2">21:00</label>
+								</div>
+
+								{/* <input type="time" name="time" placeholder="Time" /> */}
+								<textarea placeholder="Message"/>
+								<input type="button" value="Add Reservation" />
+							</form>
+						</div>
+				</div>
         )
     }
 }

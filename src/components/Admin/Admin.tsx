eb2e5cec6104cb2fdca7e9d4d25b72828897a3fa
Jakob Edéus <jakob@edeus.se>
2019-09-06 11:39:08 +0200

    fix minor syntax CB FW

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -6,7 +6,6 @@ const axios = require('axios');
 interface IAdminState {
 	reservations: IAdminDetails[],
 	showEditReservation: boolean,
-	formDisplay: boolean,
 }
 
 export interface IAdminDetails {
@@ -31,19 +30,11 @@ class Admin extends React.Component<{}, IAdminState>  {
 		this.state = {
 			reservations: [],
 			showEditReservation: false,
-			formDisplay: false,
 		}
 
-		this.toggleForm = this.toggleForm.bind(this);
 		this.removeReservation = this.removeReservation.bind(this);
 		this.editReservation = this.editReservation.bind(this);
 		this.handleEditBooking = this.handleEditBooking.bind(this);
-	}
-
-	toggleForm() {
-		// this.setState({
-		// 	formDisplay: !this.state.formDisplay
-		// })
 	}
 
 	componentDidMount() {
@@ -81,17 +72,16 @@ class Admin extends React.Component<{}, IAdminState>  {
 			reservations: [...reservations],
 			showEditReservation: true
 		}, () => {
-			// console.log(this.state)
+
 		});
 	}
 
-	handleEditBooking =(dataToBeEdited: any)=> {
+	handleEditBooking = (dataToBeEdited: any) => {
 		console.log(dataToBeEdited);
 
 		axios.put(this.bookingEditUrl, dataToBeEdited, {
 			headers: { 'Content-Type': 'text/plain;' },
 		}).then((response: any) => {
-			// alert("hej")
 			console.log("Edit booking saved")
 		}).catch((error: any) => {
 			console.log(error)
@@ -99,48 +89,15 @@ class Admin extends React.Component<{}, IAdminState>  {
 	}
 
 	render() {
-		// console.log("State", this.state.reservations)
-		// console.log(this.state)
+
 		return (
-			<div>
-				{/* <div className={
-					'form-container ' +
-					(this.state.formDisplay ? '' : 'add-reservation')
-				}>
-					<div className="res-addheading" onClick={this.toggleForm}>
-						{/* <h2> <FaPlus /> Add Reservation</h2> */}
-					</div>
-
-					<div className="form-body">
-						<form>
-							<input type="text" name="name" placeholder="Full Name" />
-							<input type="text" name="phone" placeholder="Phone Number" />
-							<input type="email" name="email" placeholder="Email" />
-							<input type="date" name="date" placeholder="Date" />
-
-							<div className="inputGroup">
-								<input id="radio1" name="radio" type="radio" />
-								<label htmlFor="radio1">18:00</label>
-							</div>
-							<div className="inputGroup">
-								<input id="radio2" name="radio" type="radio" />
-								<label htmlFor="radio2">21:00</label>
-							</div>
-
-							{/* <input type="time" name="time" placeholder="Time" /> */}
-				{/* <textarea placeholder="Message" />
-							<input type="button" value="Add Reservation" />
-						</form>
-					</div> */}
-				{/* </div> } */}
-				<ListBookings
-					reservationProps={this.state.reservations}
-					removeReservation={this.removeReservation}
-					editReservationProps={this.state.showEditReservation}
-					editReservationState={this.editReservation}
-					submitBooking={this.handleEditBooking}
-				/>
-			</div>
+			<ListBookings
+				reservationProps={this.state.reservations}
+				removeReservation={this.removeReservation}
+				editReservationProps={this.state.showEditReservation}
+				editReservationState={this.editReservation}
+				submitBooking={this.handleEditBooking}
+			/>
 		)
 	}
 }

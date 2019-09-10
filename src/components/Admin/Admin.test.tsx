4b2700c264eb122d98baa25af64d994db0a3daf5
Jakob Edéus <jakob@edeus.se>
2019-09-10 13:09:15 +0200

    test for admin setstate CB FW

---
+++ src/components/Admin/Admin.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+import Admin from './Admin';
+import IAdminState from "./Admin";
+
+configure({ adapter: new Adapter() });
+
+let mockData = [{
+    CustomerID: "32",
+    Guests: "4",
+    Date: "2019-08-13",
+    Time: "21:00:00",
+    Name: "test",
+    Email: "jafha@gmail.com",
+    Phone: "0723423340",
+    ReservationID: "4",
+    isInEditMode: false
+}]
+
+
+it("fetches data and updates state", async () => {
+    const wrapper = shallow<Admin, {}, IAdminState>(<Admin />);
+    expect(wrapper.state("reservations")).toEqual([]);
+  
+    wrapper.instance().setState({
+      reservations: mockData
+    }, () => {
+        expect(wrapper.instance().state.reservations[0].ReservationID).toBe("4");
+    });
+  
+    
+  });

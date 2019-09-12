58b34f4b437846e9fdfed5098d9ab3f4fe916770
Jakob Edéus <jakob@edeus.se>
2019-09-12 11:29:02 +0200

    change name of restaurant

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ class Navbar extends React.Component<{}, {}>  {
 
 					<nav className="navbar">
             <div className="link-group">
-              <Link className="navbar-brand" to="/" >HOME</Link>
+              <Link className="navbar-brand" to="/" >セバスチャン</Link>
 							<NavLink className="nav-item nav-link" activeClassName="active" to="/about">About</NavLink>
 							<NavLink className="nav-item nav-link" activeClassName="active" to="/admin">Admin</NavLink>
 							<Link className="nav-item nav-link btn-booking" to="/booking">Reserve a table</Link>
